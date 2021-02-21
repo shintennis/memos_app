f36a81838bdf3b3c32511b025e581211520436bc
shin_GitHub <shin.tennis.1126@gmail.com>
2021-02-21 07:51:55 +0000

    commit_2

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
     try {
-        $db = new PDO('mysql:dbname=memos_app_db;host=localhost;charset=utf8',
-        'root', 'root');
+        $db = new PDO('mysql:dbname=memos_db;host=localhost;charset=utf8;unix_socket=/var/lib/mysql/mysql.sock',
+        'root', 'Sinteni_1126');
     } catch(PDOException $e){
         echo 'DB接続エラー : ' . $e->getMessage();
     }
