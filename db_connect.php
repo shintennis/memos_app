c26e5f5ddaef55a8afe2978c9d195876eba1baa8
shin_GitHub <shin.tennis.1126@gmail.com>
2021-04-08 15:13:49 +0000

    db_info

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,8 @@
 <?php
-    try {
-        $db = new PDO('mysql:dbname=memos_db;host=memos-web.chte87d6gkzh.ap-northeast-1.rds.amazonaws.com;charset=utf8;',
-        'memos_db', 'Sinteni1126');
+require('../../my_db_info.dat');
+
+try {
+        $db = new PDO('mysql:dbname=$db_name;host=$db_host;charset=utf8;','$db_user', '$db_pass');
     } catch(PDOException $e){
         echo 'DB接続エラー : ' . $e->getMessage();
     }
